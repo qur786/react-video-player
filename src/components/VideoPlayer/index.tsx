97a66b3cae95c68962e238fa08cec5f02736343c
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-13 23:22:34 +0530

    feat: added basic VideoPlayer component

---
+++ src/components/VideoPlayer/index.tsx
@@ -0,0 +1,25 @@
+import { getVideoMIMETypeFromURL } from "./utils";
+
+/**
+ * Props for VideoPlayer component.
+ */
+interface VideoPlayerProps {
+  /**
+   * Source link for the video(s).
+   */
+  sources: string | string[];
+}
+
+/**
+ * React component to play video.
+ */
+export function VideoPlayer({ sources }: VideoPlayerProps): JSX.Element {
+  return (
+    <video controls>
+      {(Array.isArray(sources) ? sources : [sources]).map((src) => (
+        <source key={src} src={src} type={getVideoMIMETypeFromURL(src)} />
+      ))}
+      <p>Your browser does not support video.</p>
+    </video>
+  );
+}
