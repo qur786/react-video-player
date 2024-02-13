2ee8033480c11bb0fb3367496bf9384f91d79506
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-14 00:09:49 +0530

    refactor: make VideoPlayer component to take its parent width

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import { VideoPlayer } from "./components/VideoPlayer";
 
 export function App(): JSX.Element {
   return (
-    <VideoPlayer sources="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+    <div className="h-full flex flex-col gap-4 py-4">
+      <h2 className="text-center text-2xl text-sky-500 font-bold">
+        React Video Player
+      </h2>
+      <div className="w-full flex flex-col md:flex-row gap-4 p-2">
+        <div className="w-full md:w-3/5 h-3/5 box-border">
+          <VideoPlayer sources="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+        </div>
+        <div>Video list</div>
+      </div>
+    </div>
   );
 }
