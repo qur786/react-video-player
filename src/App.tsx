f2d8ae1cfb9baad1b894b5b773c1cc87d432e41a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-14 23:22:54 +0530

    refactor: scroll video player into view on mobile screen view when a playlist is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import { VIDEOS } from "./data";
 import { VideoPlayer } from "./components/VideoPlayer";
 import { VideoPlaylist } from "./components/VideoPlaylist";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export function App(): JSX.Element {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const videoContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleVideoItemClick = (index: number) => {
+    setCurrentVideoIndex(index);
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      videoContainerRef.current?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className="h-screen flex flex-col gap-4 py-4 box-border">
       <h2 className="text-center text-2xl text-sky-500 font-bold">
         React Video Player
       </h2>
       <div className="w-full flex flex-col md:gap-4 md:flex-row box-border px-4">
-        <div className="w-full md:w-3/5 md:h-[90%] box-border">
+        <div
+          className="w-full md:w-3/5 md:h-[90%] box-border"
+          ref={videoContainerRef}
+        >
           <VideoPlayer {...VIDEOS[currentVideoIndex]} />
         </div>
         <div className="flex flex-col gap-4 items-center md:h-[75vh] overflow-y-auto scroll-smooth custom-scroll">
-          <VideoPlaylist videos={VIDEOS} onClick={setCurrentVideoIndex} />
+          <VideoPlaylist videos={VIDEOS} onClick={handleVideoItemClick} />
         </div>
       </div>
     </div>
