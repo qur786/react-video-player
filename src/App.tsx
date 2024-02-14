742f220ffdfd24264868dad90e5ade6355c248b3
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-14 17:34:34 +0530

    refactor: added scrolling for playlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { VideoPlayer } from "./components/VideoPlayer";
+import { VideoPlaylist } from "./components/VideoPlaylist";
 
 export function App(): JSX.Element {
   return (
@@ -10,7 +11,40 @@ export function App(): JSX.Element {
         <div className="w-full md:w-3/5 h-[90%] box-border">
           <VideoPlayer sources="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
         </div>
-        <div>Video list</div>
+        <div className="flex flex-col gap-4 md:h-[75vh] overflow-y-auto scroll-smooth custom-scroll">
+          <VideoPlaylist
+            videos={[
+              {
+                thumbnail:
+                  "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+                title: "Planet of the apes",
+                description:
+                  "Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference. Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference.",
+              },
+              {
+                thumbnail:
+                  "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+                title: "Planet of the apes",
+                description:
+                  "Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference. Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference.",
+              },
+              {
+                thumbnail:
+                  "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+                title: "Planet of the apes",
+                description:
+                  "Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference. Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference.",
+              },
+              {
+                thumbnail:
+                  "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+                title: "Planet of the apes",
+                description:
+                  "Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference. Player component is preferred when built in house. If you're facing any issue, feel free to use external libraries for player based on your preference.",
+              },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
