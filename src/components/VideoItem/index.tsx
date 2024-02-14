7d384d038f8d4ed2debc40b68d6e22a8a15662d7
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-14 17:43:47 +0530

    refactor: add play icon to Video Item

--- src/components/VideoItem/index.tsx
+++ src/components/VideoItem/index.tsx
@@ -1,3 +1,5 @@
+import { PlayIcon } from "@heroicons/react/16/solid";
+
 export interface VideoItemProps {
   thumbnail: string;
   title: string;
@@ -10,7 +12,8 @@ export function VideoItem({
   description,
 }: VideoItemProps): JSX.Element {
   return (
-    <div className="flex flex-row flex-shrink-0 gap-2 h-36 overflow-clip hover:cursor-pointer group snap-start">
+    <div className="flex flex-row flex-shrink-0 relative gap-2 h-36 overflow-clip hover:cursor-pointer group snap-start">
+      <PlayIcon className="h-8 z-10 group-hover:scale-150 transition-transform text-white absolute top-1/2 left-1/4 -translate-x-1/4 -translate-y-1/2" />
       <img
         src={thumbnail}
         className="w-48 rounded-lg object-cover group-hover:grayscale-[80%]"
