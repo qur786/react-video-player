fa51499795f8e89a24969af255abf04958257ce8
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-15 19:06:23 +0530

    refactor: minor

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,6 @@
-export const VIDEOS = [
+import type { VideoItem } from "./components/VideoItem";
+
+export const VIDEOS: VideoItem[] = [
   {
     description:
       "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
