efc5ed85fe05ad862e155c798de281e48a5f1f29
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-16 21:09:20 +0530

    feat: added searching feature to the play list

--- src/components/VideoItem/index.tsx
+++ src/components/VideoItem/index.tsx
@@ -13,6 +13,7 @@ export interface VideoItem {
 export interface VideoItemProps extends VideoItem {
   onClick?: (index: number) => void;
   index: number;
+  moveDisabled?: boolean;
 }
 
 export function VideoItem({
@@ -21,6 +22,7 @@ export function VideoItem({
   description,
   index,
   onClick,
+  moveDisabled,
 }: VideoItemProps): JSX.Element {
   const handleClick: MouseEventHandler<HTMLImageElement> = (e) => {
     e.stopPropagation();
@@ -35,12 +37,15 @@ export function VideoItem({
           src={thumbnail}
           data-index={index}
           onClick={handleClick}
-          className="w-48 h-full rounded-lg object-cover peer hover:grayscale-[80%]"
+          className="md:w-48 w-screen h-full rounded-lg object-cover peer hover:grayscale-[80%]"
         />
         <PlayIcon className="h-8 z-10 peer-hover:scale-150 hover:scale-150 peer transition-transform text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
       </div>
       {/* To make any entity movable by some interactive element, then we need to use data-movable-handle as per react-movable package. Ref: https://github.com/tajo/react-movable/blob/0df486a69ec122ea18229b85e0d60dc5f6091612/examples/Handle.tsx#L101 */}
-      <div className="flex flex-col w-48 cursor-move" data-movable-handle>
+      <div
+        className={`flex flex-col md:w-48 w-screen ${moveDisabled ? "cursor-no-drop" : "cursor-move"}`} // To show disabled icon for move when the user is searching
+        data-movable-handle
+      >
         <h6 className="text-lg font-bold">{title}</h6>
         <p className="text-sm opacity-60">
           {description.split(" ").slice(0, 10).join(" ")}
--- src/components/VideoPlaylist/index.tsx
+++ src/components/VideoPlaylist/index.tsx
@@ -1,5 +1,6 @@
+import type { ChangeEventHandler } from "react";
 import { VideoItem } from "../VideoItem";
-import type { Dispatch, SetStateAction } from "react";
+import { type Dispatch, type SetStateAction, useState } from "react";
 import { List, arrayMove } from "react-movable";
 
 interface VideoPlaylistProps {
@@ -13,35 +14,61 @@ export function VideoPlaylist({
   setVideos,
   onClick,
 }: VideoPlaylistProps): JSX.Element {
+  const [currentVideos, setCurrentVideos] = useState(videos);
+  const [search, setSearch] = useState(""); // For search string
+
+  const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearch(e.target.value);
+    const fileteredVideos = videos.filter((ele) =>
+      ele.title.toLocaleLowerCase().startsWith(e.target.value.toLowerCase()),
+    ); // Filtering videos from original video list and then setting it to the current video list to show users searched videos
+    setCurrentVideos(fileteredVideos);
+  };
+
   return (
-    <List
-      values={videos}
-      onChange={({ oldIndex, newIndex }) => {
-        setVideos(arrayMove(videos, oldIndex, newIndex));
-      }}
-      renderList={({ children, props }) => (
-        <ul className="overflow-y-auto" {...props}>
-          {children}
-        </ul>
-      )}
-      renderItem={({
-        value: { title, thumbnail, description, sources },
-        props,
-        index,
-      }) => {
-        return (
-          <li className="py-2 list-none" {...props} key={title}>
-            <VideoItem
-              thumbnail={thumbnail}
-              title={title}
-              description={description}
-              sources={sources}
-              index={index ?? 0}
-              onClick={onClick}
-            />
-          </li>
-        );
-      }}
-    />
+    <div className="flex flex-col gap-2 md:w-96 w-screen">
+      <input
+        id="video-search"
+        type="search"
+        placeholder="Search video by title"
+        value={search}
+        onChange={handleSearchChange}
+        className="border w-[80%] self-center box-border px-2 py-1 rounded-md border-black"
+      />
+      <List
+        values={
+          search.length > 0
+            ? currentVideos.map((ele) => ({ ...ele, disabled: true }))
+            : currentVideos // To disable moveability on search
+        }
+        onChange={({ oldIndex, newIndex }) => {
+          setVideos(arrayMove(currentVideos, oldIndex, newIndex)); // To update original video index on move
+        }}
+        renderList={({ children, props }) => (
+          <ul className="overflow-y-auto" {...props}>
+            {children}
+          </ul>
+        )}
+        renderItem={({
+          value: { title, thumbnail, description, sources },
+          props,
+          index,
+        }) => {
+          return (
+            <li className="py-2 list-none" {...props} key={title}>
+              <VideoItem
+                thumbnail={thumbnail}
+                title={title}
+                description={description}
+                sources={sources}
+                index={index ?? 0}
+                onClick={onClick}
+                moveDisabled={search.length > 0}
+              />
+            </li>
+          );
+        }}
+      />
+    </div>
   );
 }
